b94b7e3b174e3e1e78e917b6341f1ad638be6ecf
Sergei Neviadomski <johndoe@example.comneviadomski@gmail.com>
2016-10-05 00:42:52 -0400

    Beta

---
+++ Readme.md
@@ -0,0 +1,17 @@
+## Wrangle OpenStreetMap Data: Pittsburgh
+
+In this project I analyze the OpenStreetMap data of the Pittsburgh, perform automated data cleaning via python scripts, and import the data into a SQL database which can be used for querying the data.
+
+All files related to this project are located in this GitHub directory. 
+
+1) iPython notebook with all the code [here] (OpenStreetMap notebook.ipynb).
+
+2) Archive with original OSM data [here](pittsburgh_pennsylvania.osm.bz2).
+
+3) Small sample of OSM data [here](pittsburgh_pennsylvania.osm).
+
+4) Schema of transformation [here](schema.py).
+
+5) SQL file with code for creating SQLite database [here](create_db.sql).
+
+6) Readme file [here](Readme.md).
---
+++ create_db.sql
@@ -0,0 +1,54 @@
+CREATE TABLE nodes (
+    id INTEGER PRIMARY KEY NOT NULL,
+    lat REAL,
+    lon REAL,
+    user TEXT,
+    uid INTEGER,
+    version INTEGER,
+    changeset INTEGER,
+    timestamp TEXT
+);
+
+CREATE TABLE nodes_tags (
+    id INTEGER,
+    key TEXT,
+    value TEXT,
+    type TEXT,
+    FOREIGN KEY (id) REFERENCES nodes(id)
+);
+
+CREATE TABLE ways (
+    id INTEGER PRIMARY KEY NOT NULL,
+    user TEXT,
+    uid INTEGER,
+    version TEXT,
+    changeset INTEGER,
+    timestamp TEXT
+);
+
+CREATE TABLE ways_tags (
+    id INTEGER NOT NULL,
+    key TEXT NOT NULL,
+    value TEXT NOT NULL,
+    type TEXT,
+    FOREIGN KEY (id) REFERENCES ways(id)
+);
+
+CREATE TABLE ways_nodes (
+    id INTEGER NOT NULL,
+    node_id INTEGER NOT NULL,
+    position INTEGER NOT NULL,
+    FOREIGN KEY (id) REFERENCES ways(id),
+    FOREIGN KEY (node_id) REFERENCES nodes(id)
+);
+
+
+.mode csv
+.import nodes.csv nodes
+.import ways.csv ways
+.import nodes_tags.csv nodes_tags
+delete from nodes_tags where id = 'id';
+.import ways_tags.csv ways_tags
+delete from ways_tags where id = 'id';
+.import ways_nodes.csv ways_nodes
+delete from ways_nodes where id = 'id';
